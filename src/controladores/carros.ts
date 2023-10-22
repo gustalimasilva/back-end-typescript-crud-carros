fdbfbf55899b5f8e592ff1d446d859c9cba406ef
Gus <gustavolimasilva1902@gmail.com>
2023-10-22 11:00:01 -0400

    Detalhamento de carros

--- src/controladores/carros.ts
+++ src/controladores/carros.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { knex } from "../bancodedados/conexao";
+import { Carro } from "../tipos";
 
 export async function listarCarros(req: Request, res: Response) {
   try {
@@ -11,7 +12,22 @@ export async function listarCarros(req: Request, res: Response) {
   }
 }
 
-export async function detalharCarros(req: Request, res: Response) {}
+export async function detalharCarros(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    const carro = await knex<Carro>("carros")
+      .where({ id: Number(id) })
+      .first();
+
+    if (!carro) {
+      return res.status(404).json({ mensagem: "Carro não econtrado" });
+    }
+
+    return res.status(200).json(carro);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
+}
 
 export async function cadastrarCarros(req: Request, res: Response) {}
 

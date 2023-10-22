8d99b05a605699abac2b59f57cb829d5f064760f
Gus <gustavolimasilva1902@gmail.com>
2023-10-22 10:39:06 -0400

    Criação das rotas

---
+++ src/controladores/carros.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+
+export async function listarCarros(req: Request, res: Response) {
+
+}
+
+export async function detalharCarros(req: Request, res: Response) {
+  
+}
+
+export async function cadastrarCarros(req: Request, res: Response) {
+  
+}
+
+export async function atualizarCarros(req: Request, res: Response) {
+  
+}
+
+export async function excluirCarros(req: Request, res: Response) {
+  
+}
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import ('dotenv').config()
+require("dotenv").config();
 import express from "express";
+import rotas from "./rotas";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(rotas);
+
 app.listen(3000, () => {
-  console.log(`\n SERVIDOR EM PÉ NA PORTA 3000 \n`);
+  console.log(
+    `------------------------------\n SERVIDOR EM PÉ NA PORTA 3000 \n------------------------------`
+  );
 });

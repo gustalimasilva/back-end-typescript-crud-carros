282aca9d83a47970b9ba9407b29f1c1f8d5b882d
Gus <gustavolimasilva1902@gmail.com>
2023-10-22 10:52:11 -0400

    listagem de carros

--- src/controladores/carros.ts
+++ src/controladores/carros.ts
@@ -1,23 +1,20 @@
 import { Request, Response } from "express";
+import { knex } from "../bancodedados/conexao";
 
 export async function listarCarros(req: Request, res: Response) {
+  try {
+    const carros = await knex("carros");
 
+    return res.status(200).json(carros);
+  } catch (error) {
+    return res.status(500).json({ mensagem: "Erro interno do servidor" });
+  }
 }
 
-export async function detalharCarros(req: Request, res: Response) {
-  
-}
+export async function detalharCarros(req: Request, res: Response) {}
 
-export async function cadastrarCarros(req: Request, res: Response) {
-  
-}
+export async function cadastrarCarros(req: Request, res: Response) {}
 
-export async function atualizarCarros(req: Request, res: Response) {
-  
-}
+export async function atualizarCarros(req: Request, res: Response) {}
 
-export async function excluirCarros(req: Request, res: Response) {
-  
-}
-
-
+export async function excluirCarros(req: Request, res: Response) {}
